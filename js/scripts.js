be65bd55d69af7642f983bf5b3a911d196df4041
Sharon Apot <sharonapot@gmail.com>
2022-01-07 16:23:30 +0300

    Create Hover effect for the Portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,7 @@ $(document).ready(function () {
     });
     $("#desIcon").click(function () {
         $("#DesignDescription").show();
-        $("#desIcon").hide();
-
-        
+        $("#desIcon").hide();        
         $("#desIcon").attr('disabled', false);
         $("#desIcon").attr('disabled', true);
     });
@@ -70,6 +68,137 @@ $(document).ready(function () {
         $("#ProductManagementDescription").attr('disabled', false);
         $("#ProductManagementDescription").attr('disabled', true);
     });
+
+//Function for displaying text over image during hover
+
+    //Work 1
+    $('.work1text').hide();
+    $("#work1Image").animate({
+        opacity:1
+        
+    });
+
+    $("#work1Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work1text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work1text').fadeOut();
+    });
+
+    //Work 2
+    $('.work2text').hide();
+        $("#work2Image").animate({
+            opacity:1
+            
+    });
+    
+    $("#work2Image").hover(function() {
+            $(this).stop().animate({opacity:.4},200);
+            $('.work2text').fadeIn();
+    
+    }, function() {
+            $(this).stop().animate({opacity:1},500)
+            $('.work2text').fadeOut();
+    });
+
+    //Work 3
+    $('.work3text').hide();
+    $("#work3Image").animate({
+        opacity:1
+        
+    });
+
+    $("#work3Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work3text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work3text').fadeOut();
+    });
+
+    //Work 4
+    $('.work4text').hide();
+    $("#work2Image").animate({
+        opacity:1
+        
+    });
+
+    $("#work4Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work4text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work4text').fadeOut();
+    });
+
+    //Work 5
+    $('.work5text').hide();
+    $("#work5Image").animate({
+        opacity:1
+        
+    });
+
+    $("#work5Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work5text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work5text').fadeOut();
+    });
+
+    //Work 6
+    $('.work6text').hide();
+    $("#work6Image").animate({
+        opacity:1
+        
+    });
+
+    $("#work6Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work6text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work6text').fadeOut();
+    });
+
+    //Work 7
+    $('.work7text').hide();
+    $("#work7Image").animate({
+        opacity:1
+        
+    });
+
+    $("#work7Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work7text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work7text').fadeOut();
+    });
+
+    //Work 8
+    $('.work8text').hide();
+    $("#work8Image").animate({
+        opacity:1
+        
+});
+
+$("#work8Image").hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.work8text').fadeIn();
+
+}, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.work8text').fadeOut();
+});
+    
     
 });
 
